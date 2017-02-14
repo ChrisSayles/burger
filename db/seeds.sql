328ebc44bc034ffc961920787d0646e3a6d72e73
Chris Sayles <chrissayles@Chriss-MacBook-Pro-2.local>
2017-02-14 14:02:38 -0600

    Finalized Burger Eating App

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, date) VALUES ('Bacon Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Cheese Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('BBQ Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Avocado Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('BAMA Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Veggie Burger', NOW());
